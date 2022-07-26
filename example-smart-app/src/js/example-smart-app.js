5b26420c128cdf37a8ad01862c6228b256aa76ae
dinipcmba <99369352+dinipcmba@users.noreply.github.com>
2022-07-26 15:21:17 -0700

    trying to make it work

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -32,7 +32,7 @@
         $.when(pt, obv).fail(onError);
         $.when(pt, allergyIntolerance).fail(onError);
 
-       
+       var p = defaultPatient();
 
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
@@ -53,7 +53,7 @@
           var ldl = byCodes('2089-1');
           var temperature = byCodes('8310-5');
 
-          var p = defaultPatient();
+          //var p = defaultPatient();
           
           p.birthdate = patient.birthDate;
           p.gender = gender;
